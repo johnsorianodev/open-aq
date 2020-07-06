4fb3c6a674899955d99556e25d6d587172d424fe
John Rey Soriano <jmsoriano@dnl.com.ph>
2020-07-06 08:50:54 +0800

    update in swagger.yaml

--- swagger.js
+++ swagger.js
@@ -4,12 +4,12 @@ const swaggerUi = require("swagger-ui-express");
 
 const PORT = 3000;
 const app = express();
-const ticketingDocs = YAML.load("./swagger.yaml");
+const swaggerDocs = YAML.load("./swagger.yaml");
 
 var defaultOption = {
   explorer: true,
 };
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(ticketingDocs, defaultOption));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs, defaultOption));
 
 app.listen(PORT, () => console.log(`swagger running on port ${PORT}`));
